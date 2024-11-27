34df8a59c5d5e343c35ea9bc4567c0a97a1752bc
Iris <iris.dettori@gmail.com>
2024-11-27 11:49:11 +0100

    correction contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -19,22 +19,29 @@ class ContactController extends AbstractController
     public function contact(Request $request): Response
     {
 
-        //je récupère les données du formulaire en POST
-        //d'abord je récupère tout le formulaire
-        //ensuite je récupère le nom pour pouvoir le mettre a coté de mon message
-        $contact = $request->request->get('contact');
-        $nom = $request -> request -> get ('nom');
 
-        //j'initie un message a null
+        //j'initie un message, un nom, un prenom a null
         $message = null;
+        $nom = null;
+        $prenom = null;
+
 
         //si la requête est POST
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if ($request->isMethod('POST')) {
+            //je récupère les données du formulaire en POST
+            //d'abord je récupère tout le formulaire
+            //ensuite je récupère le nom pour pouvoir le mettre a coté de mon message
+            $nom = trim($request->request->get('nom'));
+            $prenom = trim($request->request->get('prenom'));
+            $contenuMessage = trim($request->request->get('message'));
 
-            //et si la clé nom existe
-            if(key_exists('nom', $_POST)){
-                //alors j'affiche le message ci dessous
-                $message = 'Message bien envoyé';
+
+            //si les champs ne sont pas vide, alors je renvoie mon message de validation
+            if (!empty($nom) && !empty($prenom) && !empty($contenuMessage)) {
+                $message = 'Message bien envoyé'.' '.$nom .' ' .$prenom;
+                //sinon, j'envoie mon message de non validation
+            } else{
+                $message = 'Veuillez remplir tous les champs'.' ' .$nom .' ' .$prenom;
             }
 
         }
@@ -43,9 +50,9 @@ class ContactController extends AbstractController
         return $this->render('contact.html.twig', [
             //je dois récupérer également ces élément de mon controller pour qu'il soit possible de les appeler
             //dans mon twig
-            'contact' => $contact,
             'message' => $message,
-            'nom' => $nom
+            'nom' => $nom,
+            'prenom' => $prenom
         ]);
     }
 }
